cebd2c177693e6bb5355d1ef80014f15bea36dbb
Muhammad Amjad <amjad.theanalyst5@gmail.com>
2026-09-05 16:35:14 +0200

    chore: update gitignore and untrack PDF/cache files

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-height = input("Enter your height in cm: ")
-#print(type (height))
-height = int(height)
-#print(type (height))
-if height > 170:
-    print("The person is tall")
-if height < 170:
-    print("The person is average")
