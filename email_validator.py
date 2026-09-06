b5f66b1183447196ccb9b62e738d45edeba0519a
Muhammad Amjad <amjad.theanalyst5@gmail.com>
2026-09-06 09:47:26 +0200

    Feat(validation): complete final rule for email end characters

--- email_validator.py
+++ email_validator.py
@@ -12,15 +12,24 @@ Your Task:
 Complete the 'validate_email' function below using Python conditionals 
 (if/else statements, logical operators, and built-in string methods).
 """
-email_address = input("Please enter your email address: ")
-if len(email_address) == 0:
-    print("invalid email address")
-elif (len(email_address) > 254):
-    print("invalid email address")
-elif l
-elif email_address.count("@") != 1:
-    print("invalid email address")
-elif email_address.endswith(".com") or email_address.endswith(".org") or email_address.endswith(".net"):
-    print()
+email = input("Please enter your email address: ")
+#1. Must not be empty
+if (len(email) == 0):
+    print("Email adress must not be empty ")
+#2. Must contain '.' and '@'
+elif not('.' in email and '@' in email):
+    print("Must contain '.' and '@'")
+#3. Must contain exactly one '@' symbol
+elif email.count("@") != 1:
+    print("Must contain exactly one '@' symbol")
+#4. Must end with '.com', '.org', or '.net'
+elif not (email.endswith(".com") or email.endswith(".org") or email.endswith(".net")):
+    print("Must end with '.com', '.org', or '.net'")
+#5. Must not be longer than 254 characters
+elif (len(email) > 254):
+    print("Must not be longer than 254 characters")
+#6. Must start and end with a letter or digit
+elif not(email[0].isalnum() and email[-1].isalnum()):
+    print("Must start and end with a letter or digit")
 else:
-    print("invalid email address")
+    print("Valid email address:", email)
