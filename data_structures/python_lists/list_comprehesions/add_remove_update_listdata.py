643be395054705b7b04320621cede19df7b35a2e
Muhammad Amjad <229785857+AmjadTheAnalyst@users.noreply.github.com>
2026-09-09 01:13:33 +0200

    feat: added information

--- data_structures/python_lists/list_comprehesions/add_remove_update_listdata.py
+++ data_structures/python_lists/list_comprehesions/add_remove_update_listdata.py
@@ -21,3 +21,18 @@ print(delivery_system[1][0])
 #what i want to add mango just after juice truck 1 and shelf 1
 delivery_system[1][1].insert(1, "Mango")
 print(delivery_system[1][1])
+
+#Task 2: Add a brand new shelf containing ["Nuts", "Oatmeal"] to the end of Truck 0.
+delivery_system.insert(1, ["Nuts", "Oatmeal"])
+print(delivery_system[1])
+
+
+############# Remove Methodologies ############
+# a.clear() it will kill the whole list
+# a.remove(value) it will first matched value menthioned
+# a.pop(index number) it will remove the value at mentioned index number and return removed value
+#in pop if index number is not mentioned, by default it removes the last index value
+############# update Methodologies ############
+# a[index number] = new value
+
+                    
