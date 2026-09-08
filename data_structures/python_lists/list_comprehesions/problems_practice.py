5b59ee3384f07f61c673293a0073edb5e09a926e
Muhammad Amjad <229785857+AmjadTheAnalyst@users.noreply.github.com>
2026-09-08 11:45:23 +0200

    feat:taking square of only even numbers

--- data_structures/python_lists/list_comprehesions/problems_practice.py
+++ data_structures/python_lists/list_comprehesions/problems_practice.py
@@ -6,3 +6,13 @@ squared_numbers = [
     for n in numbers
 ]
 print(squared_numbers)
+
+#new task added, take square for only even numbers
+numbers = [1, 2, 3, 4, 5]
+# generate a new list displaying the square of each number
+squared_numbers = [
+    n*n
+    for n in numbers
+    if n % 2 == 0
+]
+print(squared_numbers)
