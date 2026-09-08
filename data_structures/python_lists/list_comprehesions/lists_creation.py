caa13dc6033a0c16788d2f6c756803920e33f2e7
Muhammad Amjad <229785857+AmjadTheAnalyst@users.noreply.github.com>
2026-09-08 13:01:29 +0200

    feat:creating lists from strings and range functions

--- data_structures/python_lists/list_comprehesions/lists_creation.py
+++ data_structures/python_lists/list_comprehesions/lists_creation.py
@@ -4,3 +4,11 @@ a = []
 name = "amjad"
 n_list = list(name)
 print(n_list)
+#list creation from range function
+r_list = list(range(1,10))
+print(r_list)
+#what will be type of first element of this list
+#it should be a numeric 
+r_list = list(range(1,10))
+print(type(r_list[0]))
+#yaa <class 'int'>
